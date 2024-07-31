d95030fd9a80ee2794b66d0abaab8c2b52e56907
IlCarlosS <myemail@example.com>
2024-07-31 16:33:18 -0600

    subida

---
+++ inventarios/src/main/java/cs/inventarios/repositorio/ProductoRepositorio.java
@@ -0,0 +1,8 @@
+package cs.inventarios.repositorio;
+
+import cs.inventarios.modelo.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Integer> {
+
+}
